a7d0e611af09ef4511cf20741bc080eb9494e4f2
Bisrat Fekede <it.bisrat.fekede@gmail.com>
2021-02-12 13:43:53 +0300

    Started migrating to Dexie.js

--- AsyncDemo/asset/js/loadPostsAPI.js
+++ AsyncDemo/asset/js/loadPostsAPI.js
@@ -5,19 +5,50 @@ const postDiv3 = document.getElementById('thePosts');
 document.addEventListener("DOMContentLoaded", () => {
     // load_fromPlaceHolder();
     loadDataNew();
+
+    const posts = document.querySelector('#thePosts')
+    const filter = document.querySelector("body > div.ui.top.attached.tabular.menu > div > div > div > input[type=text]")
+
+    // const item = document.querySelectorAll("#thePosts > div > div.content")
+
+    filter.addEventListener('keyup', (e) => {
+        const searchInput = e.target.value.toLowerCase();
+        const postHeaders = posts.getElementsByClassName('header')
+
+        Array.from(postHeaders).forEach((postHeader) => {
+            const postHeaderText = postHeader.textContent;
+
+            if (searchInput == '') {
+                postHeader.parentElement.parentElement.style.display = ''
+            }
+            if (postHeaderText.toLowerCase().indexOf(searchInput) != -1) {
+                postHeader.parentElement.parentElement.setAttribute('id', 'el')
+                // y = document.getElementById('el')
+            }
+            if (postHeader.parentElement.parentElement.getAttribute('id') != 'el') {
+                postHeader.parentElement.parentElement.setAttribute('id', 'ele')
+                z = document.querySelectorAll('#ele')
+
+                z.forEach((element) => {
+                    element.style.display = 'none'
+                })
+
+            }
+        })
+    })
 });
 
 
 //load post from fake api function 
 function load_fromPlaceHolder() {
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        let output = ''
-        data.forEach(post => {
-            output += `
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            let output = ''
+            data.forEach(post => {
+                output += `
                 <div class="item">
                     <div class="image">
                         <img src="https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60">
@@ -37,10 +68,10 @@ function load_fromPlaceHolder() {
                     </div>
                 </div>
             `
-            postDiv3.innerHTML = output;
+                postDiv3.innerHTML = output;
+            })
+
         })
-        
-    })
 
 }
 //async await
@@ -56,10 +87,10 @@ async function load_fromPlaceHolder_new() {
 function loadDataNew() {
 
     load_fromPlaceHolder_new()
-    .then((data) => {
-        let output = ''
-        data.forEach(post => {
-            output += `
+        .then((data) => {
+            let output = ''
+            data.forEach(post => {
+                output += `
                 <div class="item">
                     <div class="image">
                         <img src="https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60">
@@ -79,9 +110,9 @@ function loadDataNew() {
                     </div>
                 </div>
             `
-            postDiv3.innerHTML = output;
+                postDiv3.innerHTML = output;
+            })
+
         })
-        
-    })
-        
+
 }
